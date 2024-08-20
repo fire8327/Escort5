5b01e3c9cc51087cd24b06a82eef3cf35ff62594
fire8327 <fire83274@gmail.com>
2024-08-20 23:11:53 +0300

    upd header

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,5 +1,33 @@
 /* menu */
 $("#toggler, #overlay").click(() => {
-    $("#menu").toggleClass("max-md:top-[74px] max-md:top-0 max-md:-translate-y-full")
+    $("#menu").toggleClass("max-md:top-[129px] max-md:top-0 max-md:-translate-y-full")
     $("#overlay").toggleClass("hidden")
 })
+
+
+/* nav-slider */
+const swiper = new Swiper('.nav-slider', {
+    loop: true,
+    navigation: {
+      nextEl: '.nav-next',
+      prevEl: '.nav-prev',
+    },    
+    spaceBetween: 20,
+    breakpoints: {
+        320: {
+          slidesPerView: 1,        
+        },
+        375: {
+          slidesPerView: 2,        
+        },
+        768: {
+          slidesPerView: 4,         
+        },
+        1024: {
+          slidesPerView: 8,         
+        },
+        1280: {
+          slidesPerView: 10,         
+        }
+    }
+});
